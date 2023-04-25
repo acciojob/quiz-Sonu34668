eaf42feb6463a87d2b4e3a8068172dafe1b8b61b
Sonu34668 <130197661+Sonu34668@users.noreply.github.com>
2023-04-25 21:42:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -35,10 +35,55 @@ const quizData = [
 ];
 const quiz= document.getElementById('quiz')
 const answerEls = document.querySelectorAll('.answer')
+const questionEl = document.getElementById('question')
 const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+const submitBtn = document.getElementById('submit')
 
-const a_text = document.getElementById('a_text')
+let currentQuiz = 0;
+let score = 0;
 
-const a_text = document.getElementById('a_text')
+loadQuiz()
+function loadQuiz() {
+	deselectAnswers()
+	const currentQuizData = quizData[currentQuiz]
 
-const a_text = document.getElementById('a_text')
+	questionE1.innerText = currentQuizData.question
+	a_text.innerText = currentQuizData.a
+	b_text.innerText = currentQuizData.b
+	c_text.innerText = currentQuizData.c
+	d_text.innerText = currentQuizData.d
+}
+function deselectAnswers(){
+	answerEls.forEach(answerEl => answerEls.checked = false)
+}
+function  getSelected(){
+	let answerEl
+	answerEls.forEach(answerEl =>){
+		if(answerEl.checked){
+			answer = answerEl.id
+		}
+	}
+					  return answer
+}
+submitBtn.addEventListener('click', () => {
+	const answer = getSelected()
+	if(answer){
+		if (answer == quizData[currentQuiz].correct){
+			score++
+		}
+		currentQuiz++
+		if(currentQuiz < quizData.length){
+			loadQuiz()
+		}
+		else{
+			quiz.innerHtml ='
+				<h2>You answered ${score}/${quizData.length} questions correctly</h2>
+				button oneclick="location.reload()">Reload</button>
+				'
+		}
+	}
+}
+	)
